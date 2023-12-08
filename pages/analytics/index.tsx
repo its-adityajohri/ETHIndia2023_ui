5d798c1c314a983edc730cb8ba99a43c8d91ea5e
issa-me-sush <coolsushaan@gmail.com>
2023-12-09 00:16:49 +0530

    bridge and analytics

---
+++ pages/analytics/index.tsx
@@ -0,0 +1,36 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Decimals</title>
+        <meta
+          content="decimals ETHIndia 2023"
+          name="hack"
+        />
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+
+      <h1 className="text-3xl font-bold underline">
+Analytics
+    </h1>
+
+        <ConnectButton />
+
+      
+
+      
+
+        
+
+
+    
+    </div>
+  );
+};
+
+export default Home;
