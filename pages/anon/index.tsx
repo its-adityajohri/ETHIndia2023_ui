331b87dd7af2421f3409dfa193ea7e3b3ee26781
issa-me-sush <coolsushaan@gmail.com>
2023-12-09 17:44:54 +0530

    ui changes until create wallet done

--- pages/anon/index.tsx
+++ pages/anon/index.tsx
@@ -13,13 +13,39 @@ import {
 } from "anon-aadhaar-react";
 import { Button } from "../../components/ui/button"
 import {exportCallDataGroth16FromPCD} from 'anon-aadhaar-pcd'
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { Clipboard } from 'lucide-react';
 import createSafe from '../../lib/createSafe';
 import { sign } from 'crypto';
+const tabHeadings = {
+  account: 'Connect Wallet',
+  password: 'Go through Anon Aadhaar KYC',
+  create: 'Aadhaar Wallet Creation'
+};
+
 const Home: NextPage = () => {
+  const [activeTab, setActiveTab] = useState('account');
   const [anonAadhaar] = useAnonAadhaar();
   const { data: walletClient, isError, isLoading } = useWalletClient()
+  // This function will handle tab change
+  const handleTabChange = (newTab: string) => {
+    setActiveTab(newTab);
+  };
+
+  // This function will handle the 'Next' button click to navigate tabs
+  const handleNextClick = () => {
+    const tabKeys = Object.keys(tabHeadings);
+    const currentTabIndex = tabKeys.indexOf(activeTab);
+    const nextTabIndex = (currentTabIndex + 1) % tabKeys.length;
+    setActiveTab(tabKeys[nextTabIndex]);
+  };
+  // This function will handle the 'Previous' button click to navigate tabs
+  const handlePreviousClick = () => {
+    const tabKeys = Object.keys(tabHeadings);
+    const currentTabIndex = tabKeys.indexOf(activeTab);
+    const nextTabIndex = (currentTabIndex - 1) % tabKeys.length;
+    setActiveTab(tabKeys[nextTabIndex]);
+  };
   async function getSigner() {
     // Check if window.ethereum is available
     // @ts-ignore 
@@ -69,24 +95,40 @@ const Home: NextPage = () => {
   return (
 
     <div>
-      <div className='bg-blue-200 h-screen w-full flex flex-col  items-center p-[2rem] gap-[2rem] '>
+      <div className='bg-blue-200 h-[100rem] w-full flex flex-col  items-center p-[2rem] gap-[2rem] '>
           <h1 className="text-3xl font-bold   font-inter">
               Create Your Anon Wallet
           </h1>
-          <div className='bg-blue-400 w-11/12 h-2/4 rounded-md shadow-md flex items-center flex-col p-4 justify-center'>
-            <Tabs defaultValue="account" className="w-[400px]">
-              <TabsList>
+          <div className=' bg-blue-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-40 border border-gray-100 w-11/12 rounded-md shadow-md flex  flex-col p-[2rem] items-center'>
+          <Tabs value={activeTab} onValueChange={handleTabChange} className="w-full">
+              {/* <TabsList>
                 <TabsTrigger value="account">Connect Wallet</TabsTrigger>
                 <TabsTrigger value="password">Go through Anon Aadhaar KYC</TabsTrigger>
                 <TabsTrigger value="create">Aadhaar Wallet Creation</TabsTrigger>
-              </TabsList>
-              <TabsContent value="account"><ConnectButton/></TabsContent>
+              </TabsList> */}
+              {/* <TabsList aria-label="Manage your account">
+            {Object.entries(tabHeadings).map(([key, title]) => (
+              <TabsTrigger key={key} value={key}>{title}</TabsTrigger>
+            ))}
+          </TabsList> */}
+              <TabsContent value="account">
+                <div className='flex flex-col items-center self-center justify-between gap-[3rem] w-full'>
+                  <p className='font-inter'>Anon Aadhaar KYC - connect your wallet</p>
+                <ConnectButton/>
+                <button onClick={handleNextClick} className=" self-end bg-blue-950 text-blue-400 border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                <span className="bg-blue-400 shadow-blue-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+         {`Next ->`}
+        </button>
+                </div>
+                </TabsContent>
               <TabsContent value="password">
-                <div>
+              <div className='flex flex-col items-center self-center justify-between gap-[3rem] w-full'>
+              <p className='font-inter'>Create Anon KYC Proof</p>
                   <LogInWithAnonAadhaar />
                   <p>{anonAadhaar?.status}</p>
-                </div>
+                
                 <div >
+                </div>
                   {/* Render the proof if generated and valid */}
                   {anonAadhaar?.status === "logged-in" && (
                     <>
@@ -97,12 +139,26 @@ const Home: NextPage = () => {
                       </button>
                     </>
                     )}
+
+<button onClick={handleNextClick} className=" self-end bg-blue-950 text-blue-400 border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                <span className="bg-blue-400 shadow-blue-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+         {`Next ->`}
+        </button>
                 </div>
+                
               </TabsContent>
               <TabsContent value="create">
+              <div className='flex flex-col items-center self-center justify-between gap-[3rem] w-full'>
+              <p className='font-inter'>Create Anon KYC Proof</p>
                 <Button onClick={() => callCreate(anonAadhaar)}>create Wallet</Button>
+                <button onClick={handlePreviousClick} className=" self-start bg-blue-950 text-blue-400 border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                <span className="bg-blue-400 shadow-blue-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+         {`<- Previous`}
+        </button>
+                </div>
               </TabsContent>
             </Tabs>
+           
           </div>
       </div>
     </div>  
